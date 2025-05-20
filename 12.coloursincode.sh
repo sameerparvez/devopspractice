033a54c97950e68712c2bcaf7f0d13565fffe59f
sameer <samparvez.shaik@gmail.com>
2025-05-20 14:52:26 +0530

    12.colourscode.sh

---
+++ 12.coloursincode.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+Softwares = ("nginx" "mysql" "python3")
+ R = "\e[31m"
+ G = "\e[32m"
+ 
+ validateuser() {
+    if [id -u -eq 0]
+    then 
+ echo -e "$G using root user"
+
+else 
+echo -e "$R use root user"
+fi
+    }
+
+ validatesoftware() {
+ if [$1 -eq 1]
+ then
+ echo -e "$G installing $2"
+    else
+    echo -e "$R $2 is already installed"
+    fi
+    }
+ 
+ validateuser   
+ dnf list installed nginx
+ validatesoftware $? "nginx"
+ sudo dnf install nginx -y
+
+validateuser
+ dnf list installed mysql
+ validatesoftware $? "mysql"
+ sudo dnf install mysql -y
+
+validateuser
+ dnf list installed python3
+ validatesoftware $? "python3"
+    sudo dnf install python3 -y
